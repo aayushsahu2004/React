0369d953d8d7d8fef39957ae95a60032c980d907
aayushsahu2004 <sahuaayush2999@gmail.com>
2024-03-12 17:23:31 +0530

    BoilerPlate/env/git

---
+++ app.js
@@ -0,0 +1,7 @@
+require('dotenv').config({path: './.env'})
+const express = require('express');
+const app = express();
+
+app.listen(process.env.PORT, () => {
+    console.log(`The server is running on port ${process.env.PORT}`);
+})
