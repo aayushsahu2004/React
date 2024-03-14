d1b6a06a77c8b31f6535a2568c5bc49e38067e53
aayushsahu2004 <sahuaayush2999@gmail.com>
2024-03-14 17:32:33 +0530

    Router Create and Set-Up

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-require('dotenv').config({path: './.env'})
+require('dotenv').config({ path: './.env' })
 const express = require('express');
 const app = express();
 
+// Db connection
 require('./models/dbconfig').dbConnection();
+
+const userRouter = require('./routes/userRoutes');
+
+// logger
+const logger = require('morgan');
+app.use(logger('tiny'));
+//Body Pasrser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/user', userRouter);
 
 app.listen(process.env.PORT, () => {
     console.log(`The server is running on port ${process.env.PORT}`);
